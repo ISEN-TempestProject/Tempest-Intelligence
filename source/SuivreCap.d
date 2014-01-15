15370ab3018cdae31c037f5c118f6335521c9e87
Crom (Thibaut CHARLES) <crom29@hotmail.fr>
2014-01-15 13:51:08 +0100

    Removed vibe.d for better build times

---
+++ source/SuivreCap.d
@@ -0,0 +1,31 @@
+import core.thread;
+import std.stdio;
+
+
+class SuivreCap{
+
+	this(){
+		m_thread = new Thread(&ThreadFunction);
+		m_thread.name("ThreadSuivreCap");
+		m_thread.isDaemon(true);
+	}
+
+	void StartDaemon(in int nLoopTimeMS){
+		m_nLoopTimeMS = nLoopTimeMS;
+		m_thread.start();
+	}
+
+private:
+	Thread m_thread;
+
+	void ThreadFunction(){
+		while(true){
+			writeln("Running "~typeof(this).stringof~" thread");
+
+			m_thread.sleep(dur!("msecs")(m_nLoopTimeMS));
+		}
+	}
+
+	int m_nLoopTimeMS;
+
+}
---
+++ source/main.d
@@ -0,0 +1,9 @@
+import std.stdio;
+
+
+int main(string[] args)
+{
+	writeln("Hello Sea !");
+
+	return 0;
+}
