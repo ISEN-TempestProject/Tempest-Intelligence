7b58cf66e74a99cbfde50ccec0bbfde13f30184d
Thomas Abot <abot_tom@yahoo.fr>
2014-02-19 11:21:37 +0100

    Added actuators

--- web_root/app/app.js
+++ web_root/app/app.js
@@ -1,10 +1,14 @@
 steal(
 	'jquery', 'can', 'can/control', 'can/model',
-	'app/views/sensorsList.mustache',
+	'app/views/sensorsList.mustache', 'app/views/actuatorsList.mustache',
 	'app/fixtures',
-function($, Can,  Control, Model, SensorsView){
+function($, Can,  Control, Model, SensorsView, ActuatorsView){
 
-	Sensors = Control({
+	/*
+	*	SENSORS
+	*/
+
+	Sensors = Control.extend({
 	  init: function(){
 	    this.element.html( Can.view( SensorsView, {sensors : this.options.sensors} ) );
 	  },
@@ -24,11 +28,47 @@ function($, Can,  Control, Model, SensorsView){
 	  destroy : "DELETE /sensors/{id}"
 	},{});
 
+
+	/*
+	*	ACTUATORS
+	*/
+
+	Actuators = Control.extend({
+	  init: function(){
+	    this.element.html( Can.view( ActuatorsView, {actuators : this.options.actuators} ) );
+	  },
+
+	  '.actuator .onoffswitch-checkbox click' : function(el, ev){
+	  	el.parent().parent().parent().find('button').each(function(){
+	  		var el$ = $(this);
+	  		el$.prop('disabled', !el.prop('checked'));
+	  	});
+	  }
+	});
+
+	Actuator = Model({
+	  findAll: 'GET /actuators',
+	  create  : "POST /actuators",
+	  update  : "PUT /actuators/{id}}",
+	  destroy : "DELETE /actuators/{id}"
+	},{});
+
+
+	/*
+	*	Document ready
+	*/
+
 	$(document).ready(function(){
 	  $.when(Sensor.findAll()).then(
 	    function(sensorResponse){
 	      new Sensors('#sensors', {
 	        sensors: sensorResponse[0]
+	      });
+	  });
+	  $.when(Actuator.findAll()).then(
+	    function(actuatorResponse){
+	      new Actuators('#actuators', {
+	        actuators: actuatorResponse[0]
 	      });
 	  });
 	});
--- web_root/app/fixtures/fixtures.js
+++ web_root/app/fixtures/fixtures.js
@@ -1,6 +1,10 @@
 steal('can/util/fixture', 
   function(Fixture){
 
+  /*
+  * SENSORS
+  */
+
   var SENSORS = [
     {
       id: 1,
@@ -44,9 +48,46 @@ steal('can/util/fixture',
   Fixture("DELETE /sensors/{id}", function(){
     return {};
   });
+
+  /*
+  * ACTUATORS
+  */
+
+  var ACTUATORS = [
+    {
+      id: 1,
+      name: 'Act-1',
+      value: 1337.42,
+      lowCaption: 'Hi',
+      highCaption: 'Lo',
+      emulated : false
+    },
+    {
+      id: 2,
+      name: 'Act-2',
+      value: 73.13,
+      lowCaption: 'Hi',
+      highCaption: 'Ho',
+      emulated : true
+    }
+  ];
+
+  Fixture('GET /actuators', function(){
+    return [ACTUATORS];
+  });
    
-  Fixture('GET /sensors', function(){
-    return [SENSORS];
+  id= 3;
+  Fixture("POST /actuators", function(){
+    return {id: (id++)};
+  });
+   
+  Fixture("PUT /actuators/{id}", function(){
+    return {};
+  });
+   
+  Fixture("DELETE /actuators/{id}", function(){
+    return {};
   });
 
-  });
+
+});
