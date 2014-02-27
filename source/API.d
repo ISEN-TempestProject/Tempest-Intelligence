dcfb5ab4f72758d7db28cb9eaa8122373426122f
Thomas Abot <abot_tom@yahoo.fr>
2014-02-27 17:02:26 +0100

    Control devices with REST

--- source/API.d
+++ source/API.d
@@ -4,7 +4,6 @@ import vibe.http.rest;
 import vibe.core.log;
 import vibe.data.json;
 import std.datetime;
-import std.stdio;
 import saillog;
 import hardware.hardware;
 import hardware.devices;
@@ -17,8 +16,11 @@ interface ISailAPI
 	// GET /:id/devices
 	Json getDevices(int id);
 
-	// POST /:id/devices
-	void addDevices(int id);
+	// POST /:id/value
+	void postValue(string data);
+
+	// POST /:id/emulation
+	void postEmulation(string data);
 
 	// GET /logs
 	Json getLogs();
@@ -40,54 +42,13 @@ class API : ISailAPI
 
 	Json getDevices()
 	{
-		return parseJsonString("
-		[
-		    {
-		      \"id\": 1,
-		      \"name\": \"Capt-1\",
-		      \"value\": 42.1337,
-		      \"delta\": 0.1,
-		      \"lowCaption\": \"up\",
-		      \"highCaption\": \"down\",
-		      \"emulated\" : true
-		    },
-		    {
-		      \"id\": 2,
-		      \"name\": \"Capt-2\",
-		      \"value\": 13.37,
-		      \"delta\": 0.15,
-		      \"lowCaption\": \"-\",
-		      \"highCaption\": \"+\",
-		      \"emulated\" : false
-		    },
-		    {
-		      \"id\": 3,
-		      \"name\": \"Capt-3\",
-		      \"value\": 22.29,
-		      \"delta\": 12.37,
-		      \"lowCaption\": \"lower\",
-		      \"highCaption\": \"higher\",
-		      \"emulated\" : true
-		    },
-		    {
-		      \"id\": 4,
-		      \"name\": \"Act-1\",
-		      \"value\": 13.37,
-		      \"delta\": 0.15,
-		      \"lowCaption\": \"Low\",
-		      \"highCaption\": \"High\",
-		      \"emulated\" : false
-		    },
-		    {
-		      \"id\": 5, 
-		      \"name\": \"Act-2\",
-		      \"value\": 22.29,
-		      \"delta\": 12.37,
-		      \"lowCaption\": \"Lo\",
-		      \"highCaption\": \"Hi\",
-		      \"emulated\" : true
-		    }
-		]");
+		Json devices = Json.emptyArray;
+		foreach(device; DeviceID.min+1 .. DeviceID.max)
+		{
+				devices ~= getDevices(device);
+		}
+
+		return devices;
 	}	
 
 	Json getDevices(int id_)
@@ -98,37 +59,37 @@ class API : ISailAPI
 			case DeviceID.Roll: 
 				Roll roll = Hardware.Get!Roll(id);
 				device.id = roll.id();
-				device.isEmulated = roll.isemulated();
+				device.emulated = roll.isemulated();
 				device.value = roll.value();
 				break;
 			case DeviceID.WindDir: 
 				WindDir wd = Hardware.Get!WindDir(id);
 				device.id = wd.id();
-				device.isEmulated = wd.isemulated();
+				device.emulated = wd.isemulated();
 				device.value = wd.value();
 				break;
 			case DeviceID.Compass: 
 				Compass compass = Hardware.Get!Compass(id);
 				device.id = compass.id();
-				device.isEmulated = compass.isemulated();
+				device.emulated = compass.isemulated();
 				device.value = compass.value();
 				break;
 			case DeviceID.Sail:
 				Sail sail = Hardware.Get!Sail(id);
 				device.id = sail.id();
-				device.isEmulated = sail.isemulated();
-				device.value = sail.value();
+				device.emulated = sail.isemulated();
+				device.value = to!int(sail.value());
 				break;	
 			case DeviceID.Helm:
 				Helm helm = Hardware.Get!Helm(id);
 				device.id = helm.id();
-				device.isEmulated = helm.isemulated();
+				device.emulated = helm.isemulated();
 				device.value = helm.value();
 				break;
 			case DeviceID.Gps:
 				Gps gps = Hardware.Get!Gps(id);
 				device.id = gps.id();
-				device.isEmulated = gps.isemulated();
+				device.emulated = gps.isemulated();
 				device.value = Json.emptyObject;
 				device.value.longitude = gps.value().longitude();
 				device.value.latitude = gps.value().latitude();
@@ -138,13 +99,82 @@ class API : ISailAPI
 				return parseJsonString("{}");
 		}
 
+		//common values
+		device.lowCaption = "-";
+		device.highCaption = "+";
+		device.delta = 0.1;
+
 		return device;
 
 		
 	}
 
-	void addDevices(int id){
-		SailLog.Post("Device set : " ~ to!string(id));
+	void postEmulation(string data){
+		SailLog.Post("Device set : " ~ data);
+		Json device = parseJsonString(data);
+		switch(to!ubyte(device.id)){
+			case DeviceID.Roll: 
+				Roll roll = Hardware.Get!Roll(cast(DeviceID) to!ubyte(device.id));
+				roll.isemulated(to!bool(device.emulated));
+				break;
+			case DeviceID.WindDir: 
+				WindDir wd = Hardware.Get!WindDir(cast(DeviceID) to!ubyte(device.id));
+				wd.isemulated(to!bool(device.emulated));
+				break;
+			case DeviceID.Compass: 
+				Compass compass = Hardware.Get!Compass(cast(DeviceID) to!ubyte(device.id));
+				compass.isemulated(to!bool(device.emulated));
+				break;
+			case DeviceID.Sail:
+				Sail sail = Hardware.Get!Sail(cast(DeviceID) to!ubyte(device.id));
+				sail.isemulated(to!bool(device.emulated));
+				break;	
+			case DeviceID.Helm:
+				Helm helm = Hardware.Get!Helm(cast(DeviceID) to!ubyte(device.id));
+				helm.isemulated(to!bool(device.emulated));
+				break;
+			case DeviceID.Gps:
+				Gps gps = Hardware.Get!Gps(cast(DeviceID) to!ubyte(device.id));
+				gps.isemulated(to!bool(device.emulated));
+				break;
+			default:
+				SailLog.Warning("Called unknown Device ID. No device set.");
+		}
+	}
+
+
+	void postValue(string data){
+		SailLog.Post("Device set : " ~ data);
+		Json device = parseJsonString(data);
+		switch(to!ubyte(device.id)){
+			case DeviceID.Roll: 
+				Roll roll = Hardware.Get!Roll(cast(DeviceID) to!ubyte(device.id));
+				roll.value(to!float(device.value));
+				break;
+			case DeviceID.WindDir: 
+				WindDir wd = Hardware.Get!WindDir(cast(DeviceID) to!ubyte(device.id));
+				wd.value(to!float(device.value));
+				break;
+			case DeviceID.Compass: 
+				Compass compass = Hardware.Get!Compass(cast(DeviceID) to!ubyte(device.id));
+				compass.value(to!float(device.value));
+				break;
+			case DeviceID.Sail:
+				Sail sail = Hardware.Get!Sail(cast(DeviceID) to!ubyte(device.id));
+				sail.value(to!ubyte(device.value));
+				break;	
+			case DeviceID.Helm:
+				Helm helm = Hardware.Get!Helm(cast(DeviceID) to!ubyte(device.id));
+				helm.value(to!double(device.value));
+				break;
+			case DeviceID.Gps:
+				Gps gps = Hardware.Get!Gps(cast(DeviceID) to!ubyte(device.id));
+				gps.value().longitude(to!double(device.value.longitude));
+				gps.value().latitude(to!double(device.value.latitude));
+				break;
+			default:
+				SailLog.Warning("Called unknown Device ID. No device set.");
+		}
 	}
 
 	Json getLogs(){
--- web_root/js/controllers.js
+++ web_root/js/controllers.js
@@ -9,21 +9,76 @@ sailControllers.controller('mainCtrl', [
 	function() {}
 ]);
 
-sailControllers.controller('deviceCtrl', ['$scope', 'Devices',
-	function($scope, Devices) {
-	    $scope.devices = Devices.query();
+sailControllers.controller('deviceCtrl', ['$scope', '$interval', '$http', '$log',
+	function($scope, $interval, $http, $log) {
+	    
+		$scope.getDevices = function(){	
+		    $http.get('/api/devices')
+			    .success(function(data, status, headers, config) {
+			        $scope.devices = data;
+			    })
+			    .error(function(data, status, headers, config) {
+			        $log.error('Can\'t reach devices.');
+			    });
+		}
 
 	    $scope.toggleDevice = function(id) {
 	    	var device = getById($scope.devices, id);
 	    	device.emulated = !device.emulated;
+	    	$scope.toogleEmulation(id, device.emulated);
 		}
 
 		$scope.deltaDevice = function(id, delta) {
-	    	var device = Devices.get({},{'id':id});
-	    	console.log('id : ' + id)
-	    	device.value += delta;
-	    	device.$save();
+	    	$http.get('/api/' + id + '/devices')
+			    .success(function(data, status, headers, config) {
+			    	data.value += delta;
+			    	$http.post('/api/value', {"data" : JSON.stringify(data)})
+			    		.success(function(data, status, headers, config) {
+					        $log.info('POST received : ' + data);
+					        $scope.getDevices();
+					    })
+					    .error(function(data, status, headers, config) {
+					        $log.error('Can\'t post devices.');
+					    });       
+			    })
+			    .error(function(data, status, headers, config) {
+			        $log.error('Can\'t reach device with id ' + id);
+			    });
 		}
+
+		$scope.toogleEmulation = function(id, isEmulated) {
+	    	$http.get('/api/' + id + '/devices')
+			    .success(function(data, status, headers, config) {
+			    	data.emulated = isEmulated;
+			    	$http.post('/api/emulation', {"data" : JSON.stringify(data)})
+			    		.success(function(data, status, headers, config) {
+					        $log.info('POST received : ' + data);
+					        $scope.getDevices();
+					    })
+					    .error(function(data, status, headers, config) {
+					        $log.error('Can\'t post devices.');
+					    });       
+			    })
+			    .error(function(data, status, headers, config) {
+			        $log.error('Can\'t reach device with id ' + id);
+			    });
+		}
+
+
+		$scope.refreshDevices = -1;
+
+		$scope.redev = function(){
+			if($scope.refreshing !== undefined) $interval.cancel($scope.refreshing);
+			
+			if($scope.refreshDevices > 0) {
+				$scope.refreshing = $interval(function(){
+					$scope.getDevices();
+				}, $scope.refreshDevices);
+			}
+		}
+
+		$scope.getDevices();
+		$scope.redev();
 	}
 ]);
 
