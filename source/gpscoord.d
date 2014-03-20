a7e999dc10e0d33a1e133438c7e8274c00dc73c1
Crom (Thibaut CHARLES) <crom29@hotmail.fr>
2014-03-20 17:58:15 +0100

    Everything working except GetDistanceToRoute

--- source/gpscoord.d
+++ source/gpscoord.d
@@ -11,7 +11,6 @@ unittest {
 	import std.exception;
 
 	GpsCoord cRef = GpsCoord(37.3919331, -122.043751);
-	SailLog.Post(cRef.To(GpsCoord.Unit.DecDeg));
 
 	//Creation and GPS parsing
 	GpsCoord c = GpsCoord(GpsCoord.Unit.GPS, "37 23.516 -122 02.625");
@@ -32,6 +31,29 @@ unittest {
 	assert(abs(GpsCoord(GpsCoord.Unit.DecDeg, cRef.To(GpsCoord.Unit.DecDeg)).GetDistanceTo(cRef))<1);
 	assert(abs(GpsCoord(GpsCoord.Unit.DegMinSec, cRef.To(GpsCoord.Unit.DegMinSec)).GetDistanceTo(cRef))<1);
 	assert(abs(GpsCoord(GpsCoord.Unit.GPS, cRef.To(GpsCoord.Unit.GPS)).GetDistanceTo(cRef))<1);
+
+	//Bearing checks
+	GpsCoord cOrig = GpsCoord(0, 0);
+	assert(cOrig.GetBearingTo(GpsCoord(1, 0))==0.0);
+	assert(cOrig.GetBearingTo(GpsCoord(0, 1))==90.0);
+	assert(cOrig.GetBearingTo(GpsCoord(-1, 0))==180.0);
+	assert(cOrig.GetBearingTo(GpsCoord(0, -1))==270.0);
+
+
+	//Destination points
+	assert(abs(cOrig.GetDestinationPoint(0, 111195).GetDistanceTo(GpsCoord(1, 0)))<0.1);
+	assert(abs(cOrig.GetDestinationPoint(360, 111195).GetDistanceTo(GpsCoord(1, 0)))<0.1);
+	assert(abs(cOrig.GetDestinationPoint(90, 111195).GetDistanceTo(GpsCoord(0, 1)))<0.1);
+	assert(abs(cOrig.GetDestinationPoint(180, 111195).GetDistanceTo(GpsCoord(-1, 0)))<0.1);
+	assert(abs(cOrig.GetDestinationPoint(270, 111195).GetDistanceTo(GpsCoord(0, -1)))<0.1);
+	assert(abs(cOrig.GetDestinationPoint(-90, 111195).GetDistanceTo(GpsCoord(0, -1)))<0.1);
+
+	//Distance to route checks
+	GpsCoord cA = GpsCoord(-5, 0);
+	GpsCoord cB = GpsCoord(+5, 0);
+	SailLog.Warning("===================Distance to route checks");
+	SailLog.Warning("Should be: ", GpsCoord(0, 0).GetDistanceTo(GpsCoord(0, 1)));
+	SailLog.Warning("Calculated: ", GpsCoord(0, 1).GetDistanceToRoute(cA, cB)/1000.0);
 
 	SailLog.Post("GpsCoord unittest done");
 }
@@ -131,7 +153,7 @@ struct GpsCoord {
 	/**
 		Converts the gps coordinates into the given representation
 	*/
-	string To(Unit u){
+	string To(Unit u)const{
 		final switch(u){
 			case Unit.DecDeg:
 				return format("%.15f %.15f", m_lat, m_long).strip();
@@ -171,29 +193,81 @@ struct GpsCoord {
 	}
 
 	/**
-		Gets the distance in meters from a point using haversine formula
+		Gets the orthodromic distance in meters from a point using haversine formula
 	*/
-	double GetDistanceTo(GpsCoord point){
+	double GetDistanceTo(GpsCoord point)const{
 
 		//Haversine formula
 		// http://www.movable-type.co.uk/scripts/latlong.html
-		double dLat = (point.latitude - latitude)*PI/180.0;
-		double dLon = (point.longitude - longitude)*PI/180.0;
-		double fLat1 = latitude*PI/180.0;
-		double fLat2 = point.latitude*PI/180.0;
+		double dLat = toRad(point.m_lat - m_lat);
+		double dLon = toRad(point.m_long - m_long);
+		double fLat1 = toRad(m_lat);
+		double fLat2 = toRad(point.m_lat);
 
 		double a = (sin(dLat/2.0))^^2.0 + cos(fLat1) * cos(fLat2) * (sin(dLon/2.0))^^2.0;
 
 		double c = 2.0*atan2(sqrt(a), sqrt(1-a)); //Radian distance
-		double d = EARTH_RADIUS * c; //To kilometers
+		double d = EARTH_RADIUS_M * c; //To meters
 
-		return d*1000.0;//convert in meters
+		return d;
 	}
-	double GetDistanceTo(GpsCoord A, GpsCoord B){
-		return 0;
+	
+	/**
+		Gets the Bearing (compass direction, in degrees) to reach the given point
+	*/
+	double GetBearingTo(GpsCoord point)const{
+		// http://www.movable-type.co.uk/scripts/latlong.html @ Bearing
+		double y = sin(point.m_long-m_long)*cos(point.m_lat);
+		double x = cos(m_lat)*sin(point.m_lat) - sin(m_lat)*cos(point.m_lat)*cos(point.m_long-m_long);
+
+		return (toDeg(atan2(y, x))+360.0)%360.0;
 	}
+
+	/**
+		Gets the distance in meters of the point from the route made with A and B
+		The distance is signed, wether if the point is on the left/right side.
+	*/
+	double GetDistanceToRoute(GpsCoord A, GpsCoord B)const{
+		// http://www.movable-type.co.uk/scripts/latlong.html @ Cross-track distance
+		double fBearAToC = toRad(A.GetBearingTo(this));
+		double fBearAToB = toRad(A.GetBearingTo(B));
+		SailLog.Post("DistanceAC=", A.GetDistanceTo(this));
+
+		return asin( sin(toRad(A.GetDistanceTo(this)*1000))*sin(fBearAToC-fBearAToB) )*EARTH_RADIUS_M;
+		
+		//double fBearAToC = toRad(A.GetBearingTo(this));
+		//double fBearAToB = toRad(A.GetBearingTo(B));
+		//return asin( sin(A.GetDistanceTo(this)/EARTH_RADIUS_M)*sin(fBearAToC-fBearAToB) )*EARTH_RADIUS_M;
+	}
+
+	//static GpsCoord GetRoutesIntersecton(GpsCoord a, GpsCoord b, GpsCoord A, GpsCoord B){
+	//	//TODO: Needs implementation
+	//}
+
+	/**
+		Gets the future position of the boat if it travels fDistance meters on its bearing
+	*/
+	GpsCoord GetDestinationPoint(double fBearing, float fDistance)const{
+
+		fDistance /= EARTH_RADIUS_M;
+		fBearing = toRad(fBearing);
+		float fLat1 = toRad(m_lat);
+		float fLon1 = toRad(m_long);
+
+		double fLat = asin( sin(fLat1)*cos(fDistance) + cos(fLat1)*sin(fDistance)*cos(fBearing) );
+		double fLon = fLon1 + atan2( sin(fBearing)*sin(fDistance)*cos(fLat1), cos(fDistance)-sin(fLat1)*sin(fLat) );
+
+		return GpsCoord(toDeg(fLat),toDeg(fLon));
+	}
+
 
 private:
+	static double toRad(double fDegree){
+		return fDegree*(PI/180);
+	}
+	static double toDeg(double fRad){
+		return fRad*(180/PI);
+	}
 
 	/**
 		Precompiled regexes for types recognition
@@ -205,7 +279,8 @@ private:
 	}
 
 	//Used in haversine formula
-	enum double EARTH_RADIUS = 6371.0	;
+	enum double EARTH_RADIUS_KM = 6371.0;
+	enum double EARTH_RADIUS_M = EARTH_RADIUS_KM*1000.0;
 
 	//In decimal degrees
 	double m_lat, m_long;
