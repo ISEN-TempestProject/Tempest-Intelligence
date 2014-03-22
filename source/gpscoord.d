9626058dcec205aaebe443f0d8467925e30ab72f
Crom (Thibaut CHARLES) <crom29@hotmail.fr>
2014-03-22 21:53:10 +0100

    Modified lat/long to radian to reduce toRad calls

--- source/gpscoord.d
+++ source/gpscoord.d
@@ -7,67 +7,67 @@ import std.math;
 import std.string;
 
 
-unittest {
-	import std.exception;
-
-	GpsCoord cRef = GpsCoord(37.3919331, -122.043751);
-
-	//Creation and GPS parsing
-	GpsCoord c = GpsCoord(GpsCoord.Unit.GPS, "37 23.516 -122 02.625");
-	assert(abs(c.longitude-cRef.longitude)<0.0001 && abs(c.latitude-cRef.latitude)<0.0001);
-
-	//Haversine test
-	assert(abs(cRef.GetDistanceTo(GpsCoord(37.3919331, -121.043751))-88344)<1);
-
-	//Parsing tests
-	c = GpsCoord(GpsCoord.Unit.DecDeg, "37.3919331 -122.043751");
-	assert(c.latitude == 37.3919331 && c.longitude == -122.043751);
-	c = GpsCoord(GpsCoord.Unit.DegMinSec, "13 37 42 13 07 04");
-	assert(abs(c.latitude-13.628333)<0.001 && abs(c.longitude-13.117778)<0.001);
-	c = GpsCoord(GpsCoord.Unit.GPS, "37 23.516 -122 02.625");
-	assert(abs(c.latitude-37.391933)<0.001 && abs(c.longitude+122.04375)<0.001);
-
-	//Format/parse tests using haversine
-	assert(abs(GpsCoord(GpsCoord.Unit.DecDeg, cRef.To(GpsCoord.Unit.DecDeg)).GetDistanceTo(cRef))<1);
-	assert(abs(GpsCoord(GpsCoord.Unit.DegMinSec, cRef.To(GpsCoord.Unit.DegMinSec)).GetDistanceTo(cRef))<1);
-	assert(abs(GpsCoord(GpsCoord.Unit.GPS, cRef.To(GpsCoord.Unit.GPS)).GetDistanceTo(cRef))<1);
-
-	//Bearing checks
-	GpsCoord cOrig = GpsCoord(0, 0);
-	assert(cOrig.GetBearingTo(GpsCoord(1, 0))==0.0);
-	assert(cOrig.GetBearingTo(GpsCoord(0, 1))==90.0);
-	assert(cOrig.GetBearingTo(GpsCoord(-1, 0))==180.0);
-	assert(cOrig.GetBearingTo(GpsCoord(0, -1))==270.0);
-	assert(abs(GpsCoord(12, 23).GetBearingTo(GpsCoord(1, 91))-93.7525)<0.001);
-	assert(abs(GpsCoord(50.066389, -5.714722).GetBearingTo(GpsCoord(58.643889, -3.07))-9.119722)<0.001);
-
-
-	//Destination points
-	assert(abs(cOrig.GetDestinationPoint(0, 111195).GetDistanceTo(GpsCoord(1, 0)))<0.1);
-	assert(abs(cOrig.GetDestinationPoint(360, 111195).GetDistanceTo(GpsCoord(1, 0)))<0.1);
-	assert(abs(cOrig.GetDestinationPoint(90, 111195).GetDistanceTo(GpsCoord(0, 1)))<0.1);
-	assert(abs(cOrig.GetDestinationPoint(180, 111195).GetDistanceTo(GpsCoord(-1, 0)))<0.1);
-	assert(abs(cOrig.GetDestinationPoint(270, 111195).GetDistanceTo(GpsCoord(0, -1)))<0.1);
-	assert(abs(cOrig.GetDestinationPoint(-90, 111195).GetDistanceTo(GpsCoord(0, -1)))<0.1);
-
-
-	//Distance to route checks
-	GpsCoord cA = GpsCoord(-5, 0);
-	GpsCoord cB = GpsCoord(+5, 0);
-	assert(abs(GpsCoord(0, 1).GetDistanceToRoute(cA, cB)-GpsCoord(0, 0).GetDistanceTo(GpsCoord(0, 1)))<0.1);
-	assert(abs(GpsCoord(0, -1).GetDistanceToRoute(cA, cB)+GpsCoord(0, 0).GetDistanceTo(GpsCoord(0, 1)))<0.1);
-
-
-	SailLog.Notify("GpsCoord unittest done");
-}
-
 /**
 	GPS Coordinates, handling types conversions between dms, gps, ...
 */
 struct GpsCoord {
 
+	unittest {
+		import std.exception;
+
+		GpsCoord cRef = GpsCoord(toRad(37.3919331), toRad(-122.043751));
+
+		//Creation and GPS parsing
+		GpsCoord c = GpsCoord(GpsCoord.Unit.GPS, "37 23.516 -122 02.625");
+		assert(abs(c.longitude-cRef.longitude)<0.001 && abs(c.latitude-cRef.latitude)<0.001);
+
+		//Haversine test
+		assert(abs(cRef.GetDistanceTo(GpsCoord(toRad(37.3919331), toRad(-121.043751)))-88344)<1);
+
+		//Parsing tests
+		c = GpsCoord(GpsCoord.Unit.DecDeg, "37.3919331 -122.043751");
+		assert(c.latitude == toRad(37.3919331) && c.longitude == toRad(-122.043751));
+		c = GpsCoord(GpsCoord.Unit.DegMinSec, "13 37 42 13 07 04");
+		assert(abs(c.latitude-toRad(13.628333))<0.001 && abs(c.longitude-toRad(13.117778))<0.001);
+		c = GpsCoord(GpsCoord.Unit.GPS, "37 23.516 -122 02.625");
+		assert(abs(c.latitude-toRad(37.391933))<0.001 && abs(c.longitude+toRad(122.04375))<0.001);
+
+		//Format/parse tests using haversine
+		assert(abs(GpsCoord(GpsCoord.Unit.DecDeg, cRef.To(GpsCoord.Unit.DecDeg)).GetDistanceTo(cRef))<1);
+		assert(abs(GpsCoord(GpsCoord.Unit.DegMinSec, cRef.To(GpsCoord.Unit.DegMinSec)).GetDistanceTo(cRef))<1);
+		assert(abs(GpsCoord(GpsCoord.Unit.GPS, cRef.To(GpsCoord.Unit.GPS)).GetDistanceTo(cRef))<1);
+
+		//Bearing checks
+		GpsCoord cOrig = GpsCoord(0, 0);
+		assert(cOrig.GetBearingTo(GpsCoord(1, 0))==0.0);
+		assert(cOrig.GetBearingTo(GpsCoord(0, 1))==90.0);
+		assert(cOrig.GetBearingTo(GpsCoord(-1, 0))==180.0);
+		assert(cOrig.GetBearingTo(GpsCoord(0, -1))==270.0);
+		assert(abs(GpsCoord(toRad(12), toRad(23)).GetBearingTo(GpsCoord(toRad(1), toRad(91)))-93.7525)<0.001);
+		assert(abs(GpsCoord(toRad(50.066389), toRad(-5.714722)).GetBearingTo(GpsCoord(toRad(58.643889), toRad(-3.07)))-9.119722)<0.001);
+
+
+		//Destination points
+		assert(abs(cOrig.GetDestinationPoint(0, 111195)		.GetDistanceTo(GpsCoord(toRad(1),	toRad(0))))	<0.1);
+		assert(abs(cOrig.GetDestinationPoint(360, 111195)	.GetDistanceTo(GpsCoord(toRad(1),	toRad(0))))	<0.1);
+		assert(abs(cOrig.GetDestinationPoint(90, 111195)	.GetDistanceTo(GpsCoord(toRad(0), 	toRad(1))))	<0.1);
+		assert(abs(cOrig.GetDestinationPoint(180, 111195)	.GetDistanceTo(GpsCoord(toRad(-1), 	toRad(0))))	<0.1);
+		assert(abs(cOrig.GetDestinationPoint(270, 111195)	.GetDistanceTo(GpsCoord(toRad(0), 	toRad(-1))))<0.1);
+		assert(abs(cOrig.GetDestinationPoint(-90, 111195)	.GetDistanceTo(GpsCoord(toRad(0), 	toRad(-1))))<0.1);
+
+
+		//Distance to route checks
+		GpsCoord cA = GpsCoord(toRad(-5), 0);
+		GpsCoord cB = GpsCoord(toRad(+5), 0);
+		assert(abs(GpsCoord(0, toRad(1)).GetDistanceToRoute(cA, cB)-GpsCoord(0, 0).GetDistanceTo(GpsCoord(0, toRad(1))))<0.1);
+		assert(abs(GpsCoord(0, toRad(-1)).GetDistanceToRoute(cA, cB)+GpsCoord(0, 0).GetDistanceTo(GpsCoord(0, toRad(1))))<0.1);
+
+
+		SailLog.Notify("GpsCoord unittest done");
+	}
+
 	/*!
-		@brief Constructor for decimal degrees
+		@brief Constructor for radians
 	*/
 	this(in double latitude, in double longitude) {
 		m_lat = latitude;
@@ -93,8 +93,8 @@ struct GpsCoord {
 			case Unit.DecDeg:
 				auto results = match(expr, rgxDecDeg);
 				if(results){
-					m_lat = to!double(results.captures[1]);
-					m_long = to!double(results.captures[2]);
+					m_lat = toRad(to!double(results.captures[1]));
+					m_long = toRad(to!double(results.captures[2]));
 				}
 				else
 					throw new Exception("'"~expr~"' does not match "~to!string(u)~" value");
@@ -111,17 +111,19 @@ struct GpsCoord {
 					uint nM2  = to!uint(results.captures[7]);
 					float nS2  = to!float(results.captures[8]);
 
-					m_lat = 0;
-					m_lat += nD1;
-					m_lat += (nM1/60.0);
-					m_lat += (nS1/3600.0);
-					if(cP1=="S" || cP1=="-")m_lat = -m_lat;
+					double lat = 0;
+					lat += nD1;
+					lat += (nM1/60.0);
+					lat += (nS1/3600.0);
+					if(cP1=="S" || cP1=="-")lat = -lat;
+					m_lat = toRad(lat);
 
-					m_long = 0;
-					m_long += nD2;
-					m_long += (nM2/60.0);
-					m_long += (nS2/3600.0);
-					if(cP2=="W" || cP2=="-")m_long = -m_long;
+					double lon = 0;
+					lon += nD2;
+					lon += (nM2/60.0);
+					lon += (nS2/3600.0);
+					if(cP2=="W" || cP2=="-")lon = -lon;
+					m_long = toRad(lon);
 				}
 				else
 					throw new Exception("'"~expr~"' does not match "~to!string(u)~" value");
@@ -136,15 +138,17 @@ struct GpsCoord {
 					uint nD2 = to!uint(results.captures[5]);
 					double fM2  = to!double(results.captures[6]);
 
-					m_lat = 0;
-					m_lat += nD1;
-					m_lat += (fM1/60.0);
-					if(cP1=="S" || cP1=="-")m_lat = -m_lat;
+					double lat = 0;
+					lat += nD1;
+					lat += (fM1/60.0);
+					if(cP1=="S" || cP1=="-")lat = -lat;
+					m_lat = toRad(lat);
 
-					m_long = 0;
-					m_long += nD2;
-					m_long += (fM2/60.0);
-					if(cP2=="W" || cP2=="-")m_long = -m_long;
+					double lon = 0;
+					lon += nD2;
+					lon += (fM2/60.0);
+					if(cP2=="W" || cP2=="-")lon = -lon;
+					m_long = toRad(lon);
 				}
 				else
 					throw new Exception("'"~expr~"' does not match "~to!string(u)~" value");
@@ -159,34 +163,38 @@ struct GpsCoord {
 	string To(Unit u)const{
 		final switch(u){
 			case Unit.DecDeg:
-				return format("%.15f %.15f", m_lat, m_long).strip();
+				return format("%.15f %.15f", toDeg(m_lat), toDeg(m_long)).strip();
 			case Unit.DegMinSec:
-				char c1 = m_lat<0 ? 'S' : 'N';
-				uint nD1 = to!uint(abs(m_lat));
-				uint nM1 = to!uint(abs((abs(m_lat)-nD1)*60.0));
-				float fS1 = abs((abs(m_lat)-nD1-nM1/60.0)*3600.0);
+				double lat=toDeg(m_lat), lon=toDeg(m_long);
 
-				char c2 = m_long<0 ? 'W' : 'E';
-				uint nD2 = to!uint(abs(m_long));
-				uint nM2 = to!uint(abs((abs(m_long)-nD2)*60.0));
-				float fS2 = abs((abs(m_long)-nD2-nM2/60.0)*3600.0);
+				char c1 = lat<0 ? 'S' : 'N';
+				uint nD1 = to!uint(abs(lat));
+				uint nM1 = to!uint(abs((abs(lat)-nD1)*60.0));
+				float fS1 = abs((abs(lat)-nD1-nM1/60.0)*3600.0);
+
+				char c2 = lon<0 ? 'W' : 'E';
+				uint nD2 = to!uint(abs(lon));
+				uint nM2 = to!uint(abs((abs(lon)-nD2)*60.0));
+				float fS2 = abs((abs(lon)-nD2-nM2/60.0)*3600.0);
 
 				return format("%c %d %d %.15f %c %d %d %.15f", c1, nD1, nM1, fS1, c2, nD2, nM2, fS2).strip();
 			case Unit.GPS:
-				char c1 = m_lat<0 ? 'S' : 'N';
-				uint nD1 = to!uint(abs(m_lat));
-				double fM1 = abs((abs(m_lat)-nD1)*60.0);
+				double lat=toDeg(m_lat), lon=toDeg(m_long);
 
-				char c2 = m_long<0 ? 'W' : 'E';
-				uint nD2 = to!uint(abs(m_long));
-				double fM2 = abs((abs(m_long)-nD2)*60.0);
+				char c1 = lat<0 ? 'S' : 'N';
+				uint nD1 = to!uint(abs(lat));
+				double fM1 = abs((abs(lat)-nD1)*60.0);
+
+				char c2 = lon<0 ? 'W' : 'E';
+				uint nD2 = to!uint(abs(lon));
+				double fM2 = abs((abs(lon)-nD2)*60.0);
 
 				return format("%c %d %.15f %c %d %.15f", c1, nD1, fM1, c2, nD2, fM2).strip();
 		}
 	}
 
 	/**
-		Accessor for the value in decimal degrees
+		Accessor for the value in radian
 	*/
 	@property{
 		double longitude()const{return m_long;}
@@ -196,16 +204,16 @@ struct GpsCoord {
 	}
 
 	/**
-		Gets the orthodromic distance in meters from a point using haversine formula
+		Gets the orthodromic distance (great circle) in meters to a point using haversine formula
 	*/
 	double GetDistanceTo(GpsCoord point)const{
 
 		//Using Haversine formula
 		// http://www.movable-type.co.uk/scripts/latlong.html
-		double dLat = toRad(point.m_lat - m_lat);
-		double dLon = toRad(point.m_long - m_long);
-		double fLat1 = toRad(m_lat);
-		double fLat2 = toRad(point.m_lat);
+		double dLat = point.m_lat - m_lat;
+		double dLon = point.m_long - m_long;
+		double fLat1 = m_lat;
+		double fLat2 = point.m_lat;
 
 		double a = (sin(dLat/2.0))^^2.0 + cos(fLat1) * cos(fLat2) * (sin(dLon/2.0))^^2.0;
 
@@ -215,41 +223,57 @@ struct GpsCoord {
 		return d;
 	}
 
+	/**
+		Gets the orthodromic distance (great circle) in meters to a point using the law of cosines
+	*/
 	double GetDistanceToCos(GpsCoord point){
 		//Using spherical law of cosines
 
-		return acos( sin(toRad(this.m_lat))*sin(toRad(point.m_lat)) +
-                   cos(toRad(this.m_lat))*cos(toRad(point.m_lat)) * cos(toRad(point.m_long-this.m_long)) ) * EARTH_RADIUS_M;
+		return acos( sin(this.m_lat)*sin(point.m_lat) +
+                   cos(this.m_lat)*cos(point.m_lat) * cos(point.m_long-this.m_long) ) * EARTH_RADIUS_M;
 	}
 
+	/**
+		Gets the distance in meters to a point using the Pythagoras theorem (flat approximation)
+	*/
 	double GetDistanceToPyth(GpsCoord point){
-		//Using Pythagore (correct for short distances)
-		double x = toRad(point.m_long-m_long) * cos(toRad(point.m_lat+m_lat)/2);
-		double y = toRad(point.m_lat-m_lat);
+		//Using Pythagoras (correct for short distances)
+		double x = (point.m_long-m_long) * cos((point.m_lat+m_lat)/2);
+		double y = point.m_lat-m_lat;
 		return sqrt(x*x + y*y) * EARTH_RADIUS_M;
 	}
 	
 	/**
-		Gets the Bearing (compass direction, in degrees) to reach the given point
+		Gets the Bearing (compass direction, in degrees) to reach the given point using the great circle route (shortest)
+		Note that start bearing and final bearing differs ! the great circle line is curved ;)
 	*/
 	double GetBearingTo(GpsCoord point)const{
 		// http://www.movable-type.co.uk/scripts/latlong.html @ Bearing
 
-		double rLat1 = toRad(m_lat);
-		double rLat2 = toRad(point.m_lat);
-		double rDLon = toRad(point.m_long-m_long);
+		double rLat1 = m_lat;
+		double rLat2 = point.m_lat;
+		double rDLon = point.m_long-m_long;
 
 		double y = sin(rDLon)*cos(rLat2);
 		double x = cos(rLat1)*sin(rLat2) - sin(rLat1)*cos(rLat2)*cos(rDLon);
 
 		return (toDeg(atan2(y, x))+360.0)%360.0;
 	}
+	
+	/**
+		Gets the Bearing (compass direction, in degrees) at the end of the route (great circle) from given point to this point (final bearing)
+		Note that start bearing and final bearing differs ! the great circle line is curved ;)
+	*/
+	double GetBearingFrom(GpsCoord point)const{
+		return (GetBearingTo(point)+180.0)%360.0;
+	}
 
 	/**
-		Gets the distance in meters of the point from the route made with A and B
+		Gets the distance in meters of the point from the route (great circle path) made with A and B
 		The distance is signed, wether if the point is on the left/right side.
 	*/
 	double GetDistanceToRoute(GpsCoord A, GpsCoord B)const{
+		// http://williams.best.vwh.net/avform.htm#Triangle
 		return asin(sin(toRad(A.GetBearingTo(this)-A.GetBearingTo(B))) * sin(GetDistanceTo(A)/EARTH_RADIUS_M))*EARTH_RADIUS_M;
 	}
 
@@ -264,23 +288,30 @@ struct GpsCoord {
 
 		fDistance /= EARTH_RADIUS_M;
 		fBearing = toRad(fBearing);
-		float fLat1 = toRad(m_lat);
-		float fLon1 = toRad(m_long);
+		float fLat1 = m_lat;
+		float fLon1 = m_long;
 
 		double fLat = asin( sin(fLat1)*cos(fDistance) + cos(fLat1)*sin(fDistance)*cos(fBearing) );
 		double fLon = fLon1 + atan2( sin(fBearing)*sin(fDistance)*cos(fLat1), cos(fDistance)-sin(fLat1)*sin(fLat) );
 
-		return GpsCoord(toDeg(fLat),toDeg(fLon));
+		return GpsCoord(fLat,fLon);
 	}
 
+	/**
+		Convert a radian angle to degrees
+	*/
+	static double toRad(in double fDegree){
+		return fDegree*(PI/180.0);
+	}
+
+	/**
+		Convert a degree angle to radians
+	*/
+	static double toDeg(in double fRad){
+		return fRad*(180.0/PI);
+	}
 
 private:
-	static double toRad(double fDegree){
-		return fDegree*(PI/180);
-	}
-	static double toDeg(double fRad){
-		return fRad*(180/PI);
-	}
 
 	/**
 		Precompiled regexes for types recognition
@@ -295,7 +326,7 @@ private:
 	enum double EARTH_RADIUS_KM = 6371.0;
 	enum double EARTH_RADIUS_M = EARTH_RADIUS_KM*1000.0;
 
-	//In decimal degrees
+	//In radians
 	double m_lat, m_long;
 
 }
