51881a2c86449a05241a9fadddfab2136e100f33
Crom (Thibaut CHARLES) <crom29@hotmail.fr>
2014-04-13 11:11:22 +0200

    Improved fifo, filter impl & unittests

--- source/fifo.d
+++ source/fifo.d
@@ -9,6 +9,18 @@ struct Fifo(T) {
 	this(size_t size) {
 		m_nMaxSize = size;
 	}
+	this(size_t size, ref DList!T list) {
+		m_nMaxSize = size;
+
+		int nCount;
+		foreach(ref cell ; list){
+			m_list.insertBack(cell);
+			nCount++;
+			if(nCount>=m_nMaxSize)
+				break;
+		}
+		m_nSize = nCount;
+	}
 
 	/**
 		Appends a value to the front of the fifo
@@ -47,4 +59,20 @@ private:
 	size_t m_nMaxSize;
 	size_t m_nSize = 0;
 	DList!T m_list;
+}
+
+
+unittest {
+	import saillog;
+	int GetSize(T)(ref DList!T list){
+		int i=0;
+		foreach(ref cell ; list) i++;
+		return i;
+	}
+	auto array = [0, 1, 2, 3, 4, 5, 6, 7];
+	auto complete = DList!int(array);
+	auto fifo = Fifo!int(5, complete);
+	assert(fifo.length==5 && GetSize!int(fifo.elements)==5);
+
+	SailLog.Notify("Fifo unittest done");
 }
--- source/filter.d
+++ source/filter.d
@@ -10,46 +10,83 @@ public import fifo;
 
 // TickDuration date = Clock.currAppTick();
 
+unittest {
+	import saillog;
+
+	alias Tsvf = TimestampedValue!float;
+
+	auto list = DList!Tsvf([Tsvf(TickDuration(10), 1), Tsvf(TickDuration(30), 2), Tsvf(TickDuration(60), 3), Tsvf(TickDuration(80), 4)]);
+	auto fifo = Fifo!(Tsvf)(4, list);
+
+	assert(Filter.Raw!float(fifo)==1);
+	assert(Filter.DumbAvg!float(fifo)==2.5);
+	assert(Filter.TimedAvg!float(fifo)==2.0);
+
+
+	SailLog.Notify("Filter unittest done");
+}
+
+
 alias TimestampedValue(T) = Tuple!(TickDuration,"time", T,"value");
-
-
 
 static class Filter {
 
 	public static{
 
+		/**
+			Returns the last value stored in the data
+		*/
 		T Raw(T)(ref Fifo!(TimestampedValue!T) data){
 			return data.front().value;
 		}
 
+		/**
+			Returns the average of all values stored in data
+		*/
 		T DumbAvg(T)(ref Fifo!(TimestampedValue!T) data){
 			T ret = 0;
-			foreach(ref cell ; data){
+			size_t nElements = 0;
+			foreach(ref cell ; data.elements){
+				nElements++;
 				ret += cell.value;
 			}
-			return ret/(data.length*1.0);
+			if(nElements>0)
+				return ret/(nElements*1.0);
+			return T.init;
 		}
 
+		/**
+			Returns the time-weighted average of all values stored in data
+		*/
 		T TimedAvg(T)(ref Fifo!(TimestampedValue!T) data){
-			T ret = 0;
-
 			auto rng = data.elements.opSlice();
-
 			if(!rng.empty){
+				T ret = 0;
+
+				TickDuration dt = rng.back.time-rng.front.time;
+
 				TimestampedValue!T last = rng.front;
+				rng.popFront();
 
 				for( ; !rng.empty ; rng.popFront()){
-					ret += last.value*(rng.front.time - last.time).hnsecs;
+					ret += last.value*(rng.front.time - last.time).length;
+					last = rng.front;
 				}
+
+				return ret/dt.length;
 			}
+			return 0;
 
-			//size_t size = data.length;
-			//for(int i=0 ; i<size-1 ; i++){
-			//	ret += rng[i].value*(rng[i+1].time - rng[i].time).hnsecs;
-			//}
+		}
 
-			TickDuration dt = rng.front.time - rng.back.time;
-			return ret/dt.hnsecs;
+		/**
+			Executes a kalman filter on the stored values
+		*/
+		T Kalman(T)(ref Fifo!(TimestampedValue!T) data){
+
+			//TODO : implement this !
+
+			return Raw!T(data);
 		}
 
 	}
